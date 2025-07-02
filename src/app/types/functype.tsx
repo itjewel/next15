e666c2ea1971dd66ffabb5a884b312844a60e1e3
md.jewel mia <md.jewelmia52@gmail.com>
2025-07-02 15:26:06 +0600

    type added

--- src/app/types/functype.tsx
+++ src/app/types/functype.tsx
@@ -2,3 +2,7 @@ type ReturnFuncType<T> = (...args: any[]) => T extends infer R ? R : ;
 const foo = () => 2;
 
 type Foo = ReturnFuncType<typeof foo>;
+
+type jewel = number;
+const realValue:jewel = 12
+const kamal= realValue as unknown as string
