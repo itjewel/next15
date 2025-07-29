6280b55ca973a0134e8d0bbf2cbb6b656e34b75b
md.jewel mia <md.jewelmia52@gmail.com>
2025-07-29 11:46:39 +0600

    chat added

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+import Chat from "./components/Chat";
+import ContactList from "./components/ContactList";
+
+export default function Page() {
+  const [to, setTo] = useState(contacts[0]);
+  return (
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={(pre) => setTo(pre)}
+      />
+      <Chat key={to.id} contact={to} />
+    </div>
+  );
+}
+
+const contacts = [
+  { id: 0, name: "Taylor", email: "taylor@mail.com" },
+  { id: 1, name: "Alice", email: "alice@mail.com" },
+  { id: 2, name: "Bob", email: "bob@mail.com" },
+];
